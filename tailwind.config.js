b8c88f537a9c09e3c18244baef4c81a4ad98807a
iftesam15 <143783957+iftesam15@users.noreply.github.com>
2024-10-17 11:22:00 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const { DEFAULT_FILE_SYSTEM_ADAPTER } = require('fast-glob/out/settings');
-const { transform } = require('sucrase');
+const { DEFAULT_FILE_SYSTEM_ADAPTER } = require("fast-glob/out/settings");
+const { transform } = require("sucrase");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -14,35 +14,34 @@ module.exports = {
     extend: {},
     keyframes: {
       move: {
-        "50%": { transform: "translateY(-1rem)" }
+        "50%": { transform: "translateY(-1rem)" },
       },
       rotate: {
         "0%": { transform: "rotate(0deg)" },
-        "100%": { transform: "rotate(360deg)" }
+        "100%": { transform: "rotate(360deg)" },
       },
       scaleUp: {
         "0%": { transform: "scale(0.8)" },
         "50%": { transform: "scale(1.2)" },
-        "100%": { transform: "scale(0.8)" }
-
-      }
+        "100%": { transform: "scale(0.8)" },
+      },
     },
     animation: {
       movingY: "move 3s linear infinite",
       rotating: " rotate 15s linear infinite",
-      scalingUp: "scaleUp 3s linear infinite"
+      scalingUp: "scaleUp 3s linear infinite",
     },
     fontFamily: {
       Jost: ["Jost", "sans-serif"],
-      Lobster: ["Lobster", "sans-serif"]
+      Lobster: ["Lobster", "sans-serif"],
     },
     container: {
       center: true,
       padding: {
         DEFAULT: "12px",
-        md: "32px"
-      }
-    }
+        md: "32px",
+      },
+    },
   },
   plugins: [],
-}
+};
